8fc97ca82b1704f83f1102bbd278b8190a62f287
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-01-21 21:57:22 +0100

    added debug mode and implemented pull repository

--- src/main.py
+++ src/main.py
@@ -1,26 +1,42 @@
 from repo_controller import RepoController
 
-def main(repo_path: str = None) -> None: # repo_path will be required later
-    """ Generates new dosctrings for modified parts of the code
+def main(repo_path: str = None, debug=False) -> None: # repo_path will be required later
+    """ Generates new docstrings for modified parts of the code
 
     :param repo_path: Path to the repository. Can be local or a GitHub link
     :type repo_path: str
+    :param debug: toggle debug mode
+    :type debug: boolean
     """
-    
-    # repo = RepoController(repo_path)
-    # changes = repo.get_changes()
-    # for change in changes:
-        # extract old docstring
+
+    repo = RepoController(repo_path=repo_path, debug=True)
+    changes = repo.get_changes()
+    for change in changes:
+        change_context = repo.get_context(change)
+        change_old_docstring = repo.extract_docstring(change)
+        change_code = repo.extract_code(change)
+        if change["type"] == "method":
+            args_types_exceptions = repo.extract_args_types_exceptions(change)
+            if len(args_types_exceptions["missing_types"]) > 0:
+                # inferr missing types
+                if not debug:
+                    raise NotImplementedError
+        change_dev_comments = repo.extract_dev_comments(change)
         # see if old docstring is up-to-date
-        # continue if up-to-date
-        # extract developer comments
-        # extract code without comments
-        # continue if only comments were changed
-        # generate new docstring
+        # if up-to-date:
+            # continue
+        # flag developer comments
+        # if only_comments_changed:
+            # continue
+        # generate description
+        # build docstring
         # merge new docstring with developer comments
+        # validate docstring syntax
         # insert new docstring
-    # repo.apply_changes()
-    raise NotImplementedError
+        # validate code integrity
+    repo.apply_changes()
+    if not debug:
+        raise NotImplementedError
 
 if __name__ == "__main__":
-    main()
+    main(repo_path="https://github.com/fbehrendt/bachelor_testing_repo", debug=True)
--- src/repo_controller.py
+++ src/repo_controller.py
@@ -1,37 +1,158 @@
 import configparser
 
+import validators
+from git import Repo
+import os
+import tempfile
+import shutil
+
 class RepoController():
-    def __init__(repo_path: str):
-        """
+    def __init__(self, repo_path: str, debug=False) -> None:
+        """ Initializes a RepoController Object
+
         :param repo: Path to the repository. Can be local or a GitHub link
         :type repo: str
+        :param debug: toggle debug mode
+        :type debug: boolean
         """
-        # self.repo_path = repo_path
-        # self.is_local_repo
+        self.working_dir = 'working_repo'
+        self.debug = debug
+        self.is_local_repo = validators.url(repo_path)
+        if self.is_local_repo:
+            self.repo_url = repo_path
+            self.pull_repo()
+        else:
+            self.copy_repo(repo_path)
         # self.repo = {} # {file_name: 'filename', 'methods': {'name': 'method_name', 'content': 'method content'}, 'classes': {'name': 'classname', 'methods' = {'name': 'method_name', 'content': 'method content'}}}
-        raise NotImplementedError
-    
-    def get_changes():
-        """returns changed methods, classes and modules"""
-        raise NotImplementedError
-    
-    def extract_docstring(file, class_name=None, method_name = None):
-        if class_name == None and method_name == None:
-            # extract module docstring
+        if not self.debug:
             raise NotImplementedError
-        elif class_name != None and method_name == None:
-            # extract class docstring
+    
+    def clear_working_dir(self):
+        """ Cleans the working directory"""
+        if (os.path.exists(self.working_dir)):
+            tmp = tempfile.mktemp(dir=os.path.dirname(self.working_dir))
+            shutil.move(self.working_dir, tmp)
+            shutil.rmtree(tmp)
+        os.makedirs(self.working_dir)
+
+    def pull_repo(self):
+        """ Pulls a repository into the working_repo folder"""
+        print("###MOCK### pulling remote repository")
+        self.clear_working_dir()
+        repo = Repo.clone_from(self.repo_url, self.working_dir) 
+        if not self.debug:
             raise NotImplementedError
-        elif method_name != None:
-            # extract method docstring
+    
+    def copy_repo(self, repo_path):
+        """ Copies local files into the working_repo folder
+        
+        :param repo_path: Path to the repository
+        :type repo_path: str"""
+        print("###MOCK### copying repository files to working folder")
+        self.clear_working_dir()
+        if not self.debug:
+            raise NotImplementedError
+    
+    def get_changes(self) -> list[dict]:
+        """ Returns changed methods, classes and modules
+
+        :returns: A list of changed methods/classes/modules as dicts. Keys: type, content, signature, filenames, start, end
+        :rtype: list[dict]
+        """
+        print("###MOCK### extracting changes")
+        if not self.debug:
             raise NotImplementedError
         else:
-            # throw error
+            return [{"type": "method",
+                     "content": 'def multiply(a: int,b: int): -> int\n\t"""multiply two numbers\n\n\t:param a: first number\n\t:type a: int\n\t:param b: second number\n\t:type b: int\n\t:returns: a*b\n\t:rtype: int"""\n\treturn a*b',
+                     "signature": "def multiply(a: int,b: int): -> int",
+                     "filenames": "main.py",
+                     "start": 0,
+                     "end": 9}]
+    
+    def get_context(self, code_obj) -> list[dict]:
+        """ Returns context of a given method/class/module
+        
+        :param code_obj: A dictionary with details regarding a method/class/module
+        :code_obj type: dict
+        :returns: Context of a given method/class/module
+        :rtype: list[dict]
+        """
+        print("###MOCK### Gathering context")
+        if not self.debug:
             raise NotImplementedError
+        else:
+            return [{"type": "method",
+              "content": 'def context_method_1():\n\t"""this is context method 1\n\n\t"""\n\tpass',
+              "signature": "def context_method_1():",
+              "filenames": "main.py",
+              "start": 11,
+              "end": 15}]
+    
+    def extract_docstring(self, code_obj) -> str:
+        """ Extract the docstring (if exists)
+        
+        :param code_obj: A dictionary with details regarding a method/class/module
+        :code_obj type: dict
+        :returns: docstring of the given code snippet
+        :rtype: str
+        """
+        print("###MOCK### Extracting docstring")
+        if not self.debug:
+            raise NotImplementedError
+        else:
+            return '"""multiply two numbers\n\n\t:param a: first number\n\t:type a: int\n\t:param b: second number\n\t:type b: int\n\t:returns: a*b\n\t:rtype: int"""'
+    
+    def extract_code(self, code_obj) -> str:
+        """ Extract the code without comments
+        
+        :param code_obj: A dictionary with details regarding a method/class/module
+        :code_obj type: dict
+        :returns: code snippet without comments
+        :rtype: str
+        """
+        print("###MOCK### Extracting code")
+        if not self.debug:
+            raise NotImplementedError
+        else:
+            return "return a*b"
+
+    def extract_args_types_exceptions(self, method_obj) -> str:
+        """ Extract args, types and exceptions of a given method
+        
+        :param method_obj: A dictionary with details regarding a method
+        :method_obj type: dict
+        :returns: dict with args, types and exceptions of a method as dict. Format: {params: [{name: str, type: str}], return_type: str, exceptions: list, missing_types: list}
+        :rtype: dict{list[dict]}
+        """
+        print("###MOCK### Extracting arguments, types and exceptions")
+        if not self.debug:
+            raise NotImplementedError
+        else:
+            return {"params": [{"name": "a", type: int}], "return_type": int, "exceptions": [], "missing_types": ["b"]} # multiply(a: int, b) -> int:\n\treturn a*b
+    
+    def extract_dev_comments(self, code_obj) -> list[str]:
+        """ Extract developer comments
+        
+        :param code_obj: A dictionary with details regarding a method/class/module
+        :code_obj type: dict
+        :returns: dev comments
+        :rtype: list[str]
+        """
+        print("###MOCK### Extracting developer comments")
+        if not self.debug:
+            raise NotImplementedError
+        else:
+            return "A developer comment"
             
 
-    def apply_changes():
-        """Applies the new docstring to the repo in the way configured"""
+    def apply_changes(self, changes: list|str = "all") -> None:
+        """ Applies new docstrings to the repo in the way configured in src/config.ini
+        
+        :param changes: List of changes to apply, defaults to all
+        :type changes: list|str
+        """
+        print("###MOCK### Applying changes")
         config = configparser.ConfigParser()
         config.read('src/config.ini')
         # if repo_path is local filepath:
@@ -52,4 +173,5 @@ class RepoController():
                 # push changes
             # else:
                 # raise error
-        raise NotImplementedError
+        if not self.debug:
+            raise NotImplementedError
