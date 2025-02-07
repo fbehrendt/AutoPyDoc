4c90753eb7414b5924b92e6e8b1d0d1931b49cdc
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-02-07 12:24:10 +0100

    experiments

--- src/experiments/testfile.py
+++ src/experiments/testfile.py
@@ -1,8 +1,17 @@
-import ast
+from modulefinder import ModuleFinder
 
-code = """
-if 1 == 1 and 2 == 2 and 3 == 3:
-     test = 1
-"""
-node = ast.parse(code)
-print(ast.get_source_segment(code, node))
+def main():
+     finder = ModuleFinder()
+     finder.run_script('src/experiments/create_callgraph.py')
+
+     print('Loaded modules:')
+     for name, mod in finder.modules.items():
+          print('%s: ' % name, end='')
+          print(','.join(list(mod.globalnames.keys())[:3]))
+
+          print('-'*50)
+          print('Modules not imported:')
+          print('\n'.join(finder.badmodules.keys()))
+
+if __name__ == "__main__":
+     main()
