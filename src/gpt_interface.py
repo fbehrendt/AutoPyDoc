562c6f17b4482de1c96acd9c232466f032d3300f
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-02-18 21:27:46 +0100

    finished send_batch() mock

--- src/gpt_interface.py
+++ src/gpt_interface.py
@@ -1,6 +1,32 @@
 def send_batch(batch, callback):
-    mocked_result = {
-        "id": batch["id"],
-        # TODO
-    }
-    callback(mocked_result)
+    # TODO parallelize
+    # flag developer comments
+    # if only_comments_changed:
+        # continue
+    # generate description
+    # inferr missing arg/return types
+    # generate parameter descriptions
+    # generate exception descriptions (?)
+    change_necessary = False
+    for item in batch:
+        change_necessary = not change_necessary
+        if change_necessary:
+            # option 1
+            mocked_result = {
+                "id": item["id"],
+                "no_change_necessary": False,
+                "description": "MOCK This is a docstring description.",
+                "parameter_types": {name: "MOCK type" for name in item["missing parameters"]},
+                "parameter_descriptions": {param["name"]: "MOCK description for this parameter" for param in item["parameters"]},
+                "exception_descriptions": {exception: "MOCK exception description" for exception in item["exceptions"]},
+                "return_description": "MOCK return type description"
+            }
+            if item["return missing"]:
+                mocked_result["return type"] = "MOCK return type"
+        else:
+            # option two
+            mocked_result = {
+                "id": item["id"],
+                "no_change_necessary": True,
+            }
+        callback(mocked_result)
