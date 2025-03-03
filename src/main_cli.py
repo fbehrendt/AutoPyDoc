1b64d5e51f98af72251992cc752567f4fa62ca37
nralbrecht <nralbrecht@web.de>
2025-03-03 14:04:21 +0100

    refactor: use positional arguments for main cli

--- src/main_cli.py
+++ src/main_cli.py
@@ -5,16 +5,16 @@ from main import AutoPyDoc
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run AutoPyDoc for a repository")
     parser.add_argument(
-        "-r",
-        "--repo",
+        "repo_path",
         help="Repo Path",
     )
     parser.add_argument(
-        "-t",
-        "--token",
+        "pull_request_token",
         help="Pull Request Token",
     )
     args = parser.parse_args()
 
     auto_py_doc = AutoPyDoc()
-    auto_py_doc.main(repo_path=args.repo, pull_request_token=args.token, debug=True)
+    auto_py_doc.main(
+        repo_path=args.repo_path, pull_request_token=args.pull_request_token, debug=True
+    )
