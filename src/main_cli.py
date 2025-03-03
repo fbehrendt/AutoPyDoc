28962bb8d289bec99154640349b14b7cb28d1ef3
nralbrecht <nralbrecht@web.de>
2025-03-03 14:01:40 +0100

    feat: add argparse to main cli

--- src/main_cli.py
+++ src/main_cli.py
@@ -1,6 +1,20 @@
-import sys
+import argparse
+
 from main import AutoPyDoc
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run AutoPyDoc for a repository")
+    parser.add_argument(
+        "-r",
+        "--repo",
+        help="Repo Path",
+    )
+    parser.add_argument(
+        "-t",
+        "--token",
+        help="Pull Request Token",
+    )
+    args = parser.parse_args()
+
     auto_py_doc = AutoPyDoc()
-    auto_py_doc.main(repo_path=sys.argv[1], pull_request_token=sys.argv[2], debug=True)
+    auto_py_doc.main(repo_path=args.repo, pull_request_token=args.token, debug=True)
