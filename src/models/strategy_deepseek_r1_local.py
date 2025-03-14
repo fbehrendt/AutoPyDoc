6baeeeacb73dbbc8e1a8a9db677132dd8de119de
nralbrecht <nralbrecht@web.de>
2025-03-14 15:06:58 +0100

    feat: process generated docstring output and send it to builder

--- src/models/strategy_deepseek_r1_local.py
+++ src/models/strategy_deepseek_r1_local.py
@@ -3,7 +3,12 @@ import re
 
 from gpt4all import GPT4All
 
-from gpt_input import GptInputCodeObject
+from gpt_input import (
+    GptInputCodeObject,
+    GptInputMethodObject,
+    GptOutput,
+    GptOutputMethod,
+)
 
 from .model_strategy import DocstringModelStrategy
 
@@ -46,15 +51,16 @@ class LocalDeepseekR1Strategy(DocstringModelStrategy):
                 self.logger.debug("Using prompt [%s]", prompt)
                 self.logger.info("Starting checking existing docstring")
 
-                # def generation_callback(token_id, token):
-                #     print(token, end="")
-                #     return True
+                def generation_callback(token_id, token):
+                    print(token, end="")
+
+                    return True
 
                 generated_text = self.gpt_model.generate(
                     prompt=prompt,
                     temp=0.6,
                     max_tokens=1000,
-                    # callback=generation_callback,
+                    callback=generation_callback,
                 )
 
                 self.logger.info(
@@ -69,6 +75,69 @@ class LocalDeepseekR1Strategy(DocstringModelStrategy):
                 "An error occurred while checking existing docstring", exc_info=e
             )
             raise e
+
+    def generate_docstring(self, code_object: GptInputCodeObject) -> GptOutput:
+        if isinstance(code_object, GptInputMethodObject):
+            try:
+                with self.gpt_model.chat_session():
+                    prompt = self._build_generate_docstring_prompt(code_object.code)
+
+                    self.logger.debug("Using prompt [%s]", prompt)
+                    self.logger.info("Starting docstring generation")
+
+                    def generation_callback(token_id, token):
+                        print(token, end="")
+
+                        return True
+
+                    generated_text = self.gpt_model.generate(
+                        prompt=prompt,
+                        temp=0.6,
+                        max_tokens=5000,
+                        callback=generation_callback,
+                    )
+
+                    self.logger.info(
+                        "Finished docstring generation [%s]", generated_text
+                    )
+
+                    generated_output = self._extract_generate_docstring_output(
+                        generated_text
+                    )
+
+                    return GptOutputMethod(
+                        id=code_object.id,
+                        no_change_necessary=False,
+                        description=generated_output["description"]
+                        if "description" in generated_output
+                        else "",
+                        parameter_types={
+                            p["name"]: p["type"] for p in generated_output["parameters"]
+                        }
+                        if "parameters" in generated_output
+                        else {},
+                        parameter_descriptions={
+                            p["name"]: p["description"]
+                            for p in generated_output["parameters"]
+                        }
+                        if "parameters" in generated_output
+                        else {},
+                        return_description=generated_output["returns"]["description"],
+                        return_type=generated_output["returns"]["type"],
+                        exception_descriptions={},
+                        return_missing=False,  # TODO: differentiate between missing/unkown return and gpt's output attempts
+                    )
+            except Exception as e:
+                self.logger.exception(
+                    "An error occurred during docstring generation", exc_info=e
+                )
+                # raise e
+                return GptOutput(
+                    code_object.id, no_change_necessary=True, description=""
+                )
+        else:
+            # Not Implement Yet
+            return GptOutput(code_object.id, no_change_necessary=True, description="")
 
     def _build_check_outdated_prompt(
         self, existing_docstring: str, code_content: str
@@ -115,37 +184,6 @@ Please reason step by step to find out if the existing docstring matches the cod
 
         return "matches" in analysis_json and analysis_json["matches"]
 
-    def generate_docstring(self, code_generation_input: str) -> dict:
-        try:
-            with self.gpt_model.chat_session():
-                prompt = self._build_generate_docstring_prompt(code_generation_input)
-
-                self.logger.debug("Using prompt [%s]", prompt)
-                self.logger.info("Starting docstring generation")
-
-                # def generation_callback(token_id, token):
-                #     print(token, end="")
-                #     return True
-
-                generated_text = self.gpt_model.generate(
-                    prompt=prompt,
-                    temp=0.6,
-                    max_tokens=1000,
-                    # callback=generation_callback,
-                )
-
-                self.logger.info("Finished docstring generation [%s]", generated_text)
-
-                docstring_matches = self._extract_generate_docstring_output(
-                    generated_text
-                )
-
-                return not docstring_matches
-        except Exception as e:
-            self.logger.exception(
-                "An error occurred during docstring generation", exc_info=e
-            )
-            raise e
     def _build_generate_docstring_prompt(
         self, code_content: str, language="english"
     ) -> str:
