d686d20b32dbfb572fcdfbe3c29f9b32cb1bdf02
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-03-17 17:57:28 +0100

    Updated to mock behaviour when no type or description can be generated

--- src/models/strategy_mock.py
+++ src/models/strategy_mock.py
@@ -10,6 +10,7 @@ from gpt_input import (
 )
 
 from .model_strategy import DocstringModelStrategy
+from random import randint
 
 
 class MockStrategy(DocstringModelStrategy):
@@ -25,61 +26,104 @@ class MockStrategy(DocstringModelStrategy):
         return self.change_necessary
 
     def generate_docstring(self, code_object: GptInputCodeObject) -> GptOutput:
+        if randint(0, 6) == 0:
+            description = False
+        else:
+            description = "MOCK This is a docstring description."
+
+        exception_descriptions = {}
+        for exception in code_object.exceptions:
+            if randint(0, 6) == 0:
+                exception_descriptions[exception] = False
+            else:
+                exception_descriptions[exception] = "MOCK exception description"
+
         if isinstance(code_object, GptInputMethodObject):
             return_type = None
             if code_object.return_missing:
-                return_type = "MOCK return type"
+                if randint(0, 6) == 0:
+                    return_type = False
+                else:
+                    return_type = "MOCK return type"
+
+            param_types = {}
+            for param_name in code_object.missing_parameters:
+                if randint(0, 6) == 0:
+                    param_types[param_name] = False
+                else:
+                    param_types[param_name] = "MOCK type"
+
+            param_descriptions = {}
+            for param_name in code_object.parameters:
+                if randint(0, 6) == 0:
+                    param_descriptions[param_name] = False
+                else:
+                    param_descriptions[param_name] = (
+                        "MOCK description for this parameter"
+                    )
+
+            if randint(0, 6) == 0:
+                return_description = False
+            else:
+                return_description = "MOCK return description"
 
             return GptOutputMethod(
                 id=code_object.id,
                 no_change_necessary=False,
-                description="MOCK This is a docstring description.",
-                parameter_types={
-                    name: "MOCK type" for name in code_object.missing_parameters
-                },
-                parameter_descriptions={
-                    param["name"]: "MOCK description for this parameter"
-                    for param in code_object.parameters
-                },
-                exception_descriptions={
-                    exception: "MOCK exception description"
-                    for exception in code_object.exceptions
-                },
-                return_description="MOCK return type description",
+                description=description,
+                parameter_types=param_types,
+                parameter_descriptions=param_descriptions,
+                exception_descriptions=exception_descriptions,
+                return_description=return_description,
                 return_missing=code_object.return_missing,
                 return_type=return_type,
             )
         elif isinstance(code_object, GptInputClassObject):
+            class_attribute_types = {}
+            for attr in code_object.class_attributes:
+                if randint(0, 6) == 0:
+                    class_attribute_types[attr["name"]] = False
+                else:
+                    class_attribute_types[attr["name"]] = "MOCK type"
+
+            class_attribute_descriptions = {}
+            for attr in code_object.class_attributes:
+                if randint(0, 6) == 0:
+                    class_attribute_descriptions[attr["name"]] = False
+                else:
+                    class_attribute_descriptions[attr["name"]] = (
+                        "MOCK class attr description"
+                    )
+
+            instance_attribute_types = {}
+            for attr in code_object.instance_attributes:
+                if randint(0, 6) == 0:
+                    instance_attribute_types[attr["name"]] = False
+                else:
+                    instance_attribute_types[attr["name"]] = "MOCK type"
+
+            instance_attribute_descriptions = {}
+            for attr in code_object.instance_attributes:
+                if randint(0, 6) == 0:
+                    instance_attribute_descriptions[attr["name"]] = False
+                else:
+                    instance_attribute_descriptions[attr["name"]] = (
+                        "MOCK class instance description"
+                    )
+
             return GptOutputClass(
                 id=code_object.id,
                 no_change_necessary=False,
-                description="MOCK This is a docstring description.",
-                class_attribute_descriptions={
-                    attr["name"]: "MOCK class attr description"
-                    for attr in code_object.class_attributes
-                },
-                class_attribute_types={
-                    attr["name"]: "MOCK type"
-                    for attr in code_object.class_attributes
-                    if "type" not in attr.keys()
-                },
-                instance_attribute_descriptions={
-                    attr["name"]: "MOCK instance attr description"
-                    for attr in code_object.instance_attributes
-                },
-                instance_attribute_types={
-                    attr["name"]: "MOCK type"
-                    for attr in code_object.instance_attributes
-                    if "type" not in attr.keys()
-                },
+                description=description,
+                class_attribute_descriptions=class_attribute_descriptions,
+                class_attribute_types=class_attribute_types,
+                instance_attribute_descriptions=instance_attribute_descriptions,
+                instance_attribute_types=instance_attribute_types,
             )
         elif isinstance(code_object, GptInputModuleObject):
             return GptOutputModule(
                 id=code_object.id,
                 no_change_necessary=False,
-                description="MOCK This is a docstring description.",
-                exception_descriptions={
-                    exception: "MOCK exception description"
-                    for exception in code_object.exceptions
-                },
+                description=description,
+                exception_descriptions=exception_descriptions,
             )
