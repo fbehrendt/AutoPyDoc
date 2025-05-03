32f3ba49614ca0e68a09eba42465f2063e246585
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-05-03 14:14:37 +0200

    Updated prompts and reduce context gradually if it exceed max_length. Utilize docstrings as substitutions for code.

--- src/models/strategy_deepseek_r1_local.py
+++ src/models/strategy_deepseek_r1_local.py
@@ -38,7 +38,15 @@ class DeepseekR1PromptBuilder:
         self.check_outdated_prompt_template = """
 You are an AI documentation assistant, and your task is to evaluate if an existing docstring for {code_type} {code_name} correctly describes the given code of the function.
 The purpose of the documentation is to help developers and beginners understand the code and its specific usage.
-If any part of the docstring is inadequate, consider the whole docstring to be inadequate. Any mocked docstring is to be considered inadequate. If ANY parameter/return, exception or attribute does not have a corresponding description and (apart from exceptions) type, it is to be considered inadequate.
+
+The docstring has to pass all of the following criteria to pass:
+- concise description
+- description accurately describes what the code does (rather than how)
+- all class and instance attributes are described
+- all exception raised directly (rather than in an method/subclass within the class) are described
+- docstring is not a mock
+
+Criteria not mentioned above shall not be considered! Especially, methods and subclasses do not have to be described and no examples have to be (but can be) included
 
 The code looks like the following:
 <code>
@@ -124,9 +132,9 @@ Here is an example of the expected output format:
 """
 
         self.generate_class_docstring_prompt_template = """
-You are an AI documentation assistant, and your task is to analyze the code of a Python class called {code_name}.
+You are an AI documentation assistant, and your task is to analyze the code of a Python class called {code_name}. The output will be used to construct a new docstring.
 The purpose of the analysis is to help developers and beginners understand the class and specific usage of the code.
-Use plain text (including all details), in a deterministic tone.
+Use plain text (including all details), in a deterministic tone. Provided context shall be used to better understand what the class does and does not need to be analyzed further than that. Do not generate descriptions for methods and subclasses.
 
 The code looks like the following:
 <code>
@@ -232,7 +240,7 @@ Please reason step by step, and always summarize your final answer using the fol
         prompt_length_without_context = len(
             prompt_template.format(code_name=code_object.name, code=code_object.code, context="")
         )
-        max_context_length = self.context_size - prompt_length_without_context
+        max_context_length = self.context_size - prompt_length_without_context - 50  # buffer
 
         context = self._build_context_from_code_object(code_object, max_context_length)
         self.logger.debug("Code Context length [%d/%d]", len(context), max_context_length)
@@ -309,11 +317,11 @@ Please reason step by step, and always summarize your final answer using the fol
             raw_context_summary += parent_context_summary
             context_summary += f"<related-code>\n{raw_context_summary}</related-code>\n"
 
-            biggest_context = f"""
+            full_context = f"""
 {context_summary}
 """
-            if len(biggest_context) <= max_length:
-                return biggest_context
+            if len(full_context) <= max_length:
+                return full_context
 
             medium_context = f"""
 {parent_context_summary}
@@ -321,21 +329,175 @@ Please reason step by step, and always summarize your final answer using the fol
             if len(medium_context) <= max_length:
                 return medium_context
 
-            small_context = ""
-            if len(small_context) <= max_length:
-                return small_context
+            no_context = ""
+            if len(no_context) <= max_length:
+                return no_context
 
             return code_object.code
         elif isinstance(code_object, GptInputClassObject):
-            # TODO: build context from code object
-            biggest_context = code_object.code
+            mapped_context: dict[str, list[ContextObject]] = {}
+            for key, value in code_object.context.items():
+                if value is None:
+                    continue
+                elif isinstance(value, int):
+                    mapped_context[key] = [code_object.context_objects.get(value)]
+                elif isinstance(value, Iterable):
+                    mapped_context[key] = [code_object.context_objects.get(item) for item in value]
+                else:
+                    raise Exception("Unexpected context type")
 
-            return biggest_context
+            # context as code
+            raw_context_summary = ""
+            for called_method in mapped_context["called_methods"]:
+                raw_context_summary += f"<called-method>\n{called_method.code}\n</called-method>\n"
+            for called_class in mapped_context["called_classes"]:
+                raw_context_summary += f"<called-class>\n{called_class.code}\n</called-class>\n"
+            for called_by_method in mapped_context["called_by_methods"]:
+                raw_context_summary += (
+                    f"<called-by-method>\n{called_by_method.code}\n</called-by-method>\n"
+                )
+            for called_by_class in mapped_context["called_by_classes"]:
+                raw_context_summary += (
+                    f"<called-by-class>\n{called_by_class.code}\n</called-by-class>\n"
+                )
+            for called_by_module in mapped_context["called_by_modules"]:
+                raw_context_summary += (
+                    f"<called-by-module>\n{called_by_module.code}\n</called-by-module>\n"
+                )
+            for classes in mapped_context["class_ids"]:
+                raw_context_summary += f"<sub-classes>\n{classes.code}\n</sub-classes>\n"
+            for methods in mapped_context["method_ids"]:
+                raw_context_summary += f"<class-methods>\n{methods.code}\n</class-methods>\n"
+
+            # context as docstring if exists, else code
+            raw_context_as_docstrings_or_code = ""
+            for called_method in mapped_context["called_methods"]:
+                if called_method.docstring is not None and len(called_method.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<called-method-docstring>\nMethod name:{called_method.name}\nDocstring:{called_method.docstring}\n</called-method-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<called-method>\n{called_method.code}\n</called-method>\n"
+                    )
+            for called_class in mapped_context["called_classes"]:
+                if called_class.docstring is not None and len(called_class.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<called-class-docstring>\nClass name:{called_class.name}\nDocstring:{called_class.docstring}\n</called-class-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<called-class>\n{called_class.code}\n</called-class>\n"
+                    )
+            for called_by_method in mapped_context["called_by_methods"]:
+                if called_by_method.docstring is not None and len(called_by_method.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<called-by-method-docstring>\nMethod name:{called_by_method.name}\nDocstring:{called_by_method.docstring}\n</called-by-method-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<called-by-method>\n{called_by_method.code}\n</called-by-method>\n"
+                    )
+            for called_by_class in mapped_context["called_by_classes"]:
+                if called_by_class.docstring is not None and len(called_by_class.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<called-by-class-docstring>\nClass name:{called_by_class.name}\nDocstring:{called_by_class.docstring}\n</called-by-class-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<called-by-class>\n{called_by_class.code}\n</called-by-class>\n"
+                    )
+            for called_by_module in mapped_context["called_by_modules"]:
+                if called_by_module.docstring is not None and len(called_by_module.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<called-by-module-docstring>\n{called_by_module.docstring}\n</called-by-module-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<called-by-module>\n{called_by_module.code}\n</called-by-module>\n"
+                    )
+            for classes in mapped_context["class_ids"]:
+                if classes.docstring is not None and len(classes.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<sub-classes-docstring>\nClass name:{classes.name}\nDocstring:{classes.docstring}\n</sub-classes-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<sub-classes>\n{classes.code}\n</sub-classes>\n"
+                    )
+            for methods in mapped_context["method_ids"]:
+                if methods.docstring is not None and len(methods.docstring) > 10:
+                    raw_context_as_docstrings_or_code += f"<class-methods-docstring>\nMethod name:{methods.name}\nDocstring:{methods.docstring}\n</class-methods-docstring>\n"
+                else:
+                    raw_context_as_docstrings_or_code += (
+                        f"<class-methods>\n{methods.code}\n</class-methods>\n"
+                    )
+
+            # context as docstrings
+            raw_context_as_docstrings = ""
+            for called_method in mapped_context["called_methods"]:
+                raw_context_as_docstrings += f"<called-method-docstring>\nMethod name:{called_method.name}\nDocstring:{called_method.docstring}\n</called-method-docstring>\n"
+            for called_class in mapped_context["called_classes"]:
+                raw_context_as_docstrings += f"<called-class-docstring>\nClass name:{called_class.name}\nDocstring:{called_class.docstring}\n</called-class-docstring>\n"
+            for called_by_method in mapped_context["called_by_methods"]:
+                raw_context_as_docstrings += f"<called-by-method-docstring>\nMethod name:{called_by_method.name}\nDocstring:{called_by_method.docstring}\n</called-by-method-docstring>\n"
+            for called_by_class in mapped_context["called_by_classes"]:
+                raw_context_as_docstrings += f"<called-by-class-docstring>\nClass name:{called_by_class.name}\nDocstring:{called_by_class.docstring}\n</called-by-class-docstring>\n"
+            for called_by_module in mapped_context["called_by_modules"]:
+                raw_context_as_docstrings += f"<called-by-module-docstring>\n{called_by_module.docstring}\n</called-by-module-docstring>\n"
+            for classes in mapped_context["class_ids"]:
+                raw_context_as_docstrings += f"<sub-classes-docstring>\nClass name:{classes.name}\nDocstring:{classes.docstring}\n</sub-classes-docstring>\n"
+            for methods in mapped_context["method_ids"]:
+                raw_context_as_docstrings += f"<class-methods-docstring>\nMethod name:{methods.name}\nDocstring:{methods.docstring}\n</class-methods-docstring>\n"
+
+            # context_summary += f"<related-code>\n{raw_context_summary}</related-code>\n"
+
+            parent_context_summary = ""
+            for parent in mapped_context["parent_id"]:
+                parent_context_summary += f"<part-of-code>\n{parent.code}\n</part-of-code>\n"
+            if code_object.context["parent_id"] != code_object.context["module_id"]:
+                for module in mapped_context["module_id"]:
+                    parent_context_summary += (
+                        f"<part-of-module>\n{module.code}\n</part-of-module>\n"
+                    )
+
+            parent_context_as_docstrings = ""
+            for parent in mapped_context["parent_id"]:
+                parent_context_as_docstrings += f"<part-of-code>\nCode type: {parent.code_type}\nName: {parent.name}\nDocstring: {parent.docstring}\n</part-of-code>\n"
+            if code_object.context["parent_id"] != code_object.context["module_id"]:
+                for module in mapped_context["module_id"]:
+                    parent_context_as_docstrings += (
+                        f"<part-of-module>\n{module.docstring}\n</part-of-module>\n"
+                    )
+
+            # everything as code
+            full_context = f"""<related-code>\n{raw_context_summary}{parent_context_summary}</related-code>\n"""
+            if len(full_context) <= max_length:
+                return full_context
+
+            # context as code, parent as docstring
+            full_context_parent_as_docstring = f"""<related-code>\n{raw_context_summary}{parent_context_as_docstrings}</related-code>\n"""
+            if len(full_context_parent_as_docstring) <= max_length:
+                return full_context_parent_as_docstring
+
+            # context as code, parent omitted
+            context_without_parent = f"""<related-code>\n{raw_context_summary}</related-code>\n"""
+            if len(context_without_parent) <= max_length:
+                return context_without_parent
+
+            # context as docstring, if exists, else code, parent omitted
+            context_as_docstring_or_code_without_parent = (
+                f"""<related-code>\n{raw_context_as_docstrings_or_code}</related-code>\n"""
+            )
+            if len(context_as_docstring_or_code_without_parent) <= max_length:
+                return context_as_docstring_or_code_without_parent
+
+            # context as docstring, parent omitted
+            context_without_parent_as_docstrings = (
+                f"""<related-code>\n{raw_context_as_docstrings}</related-code>\n"""
+            )
+            if len(context_without_parent_as_docstrings) <= max_length:
+                return context_without_parent_as_docstrings
+
+            # no context
+            no_context = ""
+            if len(no_context) <= max_length:
+                return no_context
+
+            return ""
         elif isinstance(code_object, GptInputModuleObject):
             # TODO: build context from code object
-            biggest_context = code_object.code
+            full_context = code_object.code
 
-            return biggest_context
+            return full_context
         else:
             raise Exception("Unexpected code object type")
 
