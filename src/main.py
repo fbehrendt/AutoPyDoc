c2bc0454fd5f6d9e33a6fad4ecaa36f9b22c81dc
Fabian Behrendt <alltheunnecessaryjunk@gmail.com>
2025-05-24 13:31:50 +0200

    set logging level according to debug flag

--- src/main.py
+++ src/main.py
@@ -51,12 +51,17 @@ class AutoPyDoc:
         :type debug: boolean
         """
 
+        # prevent runs on branches created by this tool
         if "_AutoPyDoc" in branch:
             self.logger.info("Attempt to run the tool on a branch created by it. Aborting!")
             quit()
 
+        if debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
+
         # Initialize gpt interface with the chosen strategy and its parameters early to fail early if model is unavailable or unable to load
-        self.logger.info(f"Using {model_strategy_name} strategy.")
         self.gpt_interface = GptInterface(model_strategy_name, **model_strategy_params)
 
         # pull repo, create code representation, create dependencies
